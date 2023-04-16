d0cfd1b404e5ba33da54ef43b0e0fedda332a9d7
Funnymike09 <mike.nip@mail.ru>
2023-04-17 04:43:11 +0500

    Rat family and set up for a eviroment
    h

---
+++ Assets/MikeFolder/scripts/Playercontroler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Playercontroler : MonoBehaviour
+{
+	public NavMeshAgent agent;
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(1))
+		{
+			Ray movePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(movePosition,out var hitInfo))
+			{
+				agent.SetDestination(hitInfo.point);
+			}
+		}
+	}
+}
